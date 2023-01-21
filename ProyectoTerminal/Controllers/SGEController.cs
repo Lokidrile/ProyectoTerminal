ce275d784a51906e01f8fd4ddaa181feed1fb936
Eduardo Valencia <ed.val.escom@outlook.com>
2023-01-20 21:58:59 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoTerminal/Controllers/SGEController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoTerminal.Controllers
+{
+    [Route("api/sge")]
+    [ApiController]
+    public class SGEController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/degrees")]
+        public async Task<IActionResult> GetDegreesList()
+        {
+            return null;
+        }
+    }
+}
